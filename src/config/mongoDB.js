43939a0f49c07b71da97867cbfbc6b255c226436
lamylov3 <lamnguyen.hust@gmail.com>
2021-10-18 23:53:13 +0700

    change connect DB

--- src/config/mongoDB.js
+++ src/config/mongoDB.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import { env } from "../config/environments";
+import auth from "../models/auth";
+const Role = auth.role;
 
 export const connectDB = async () => {
   const client = await mongoose.connect(env.MONGODB_URI, {
@@ -8,3 +10,38 @@ export const connectDB = async () => {
   });
   console.log("connect to mongo DB ... ");
 };
+
+export const initialDB = () => {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
-import { connectDB } from "./config/mongoDB";
+import { connectDB, initialDB } from "./config/mongoDB";
 import express from "express";
 import { env } from "./config/environments";
 import { webRouter } from "./routes/web";
-import auth from "./models/auth";
-import { authRouter } from "./routes/web/auth";
-import { userRouter } from "./routes/web/user";
 import cors from "cors";
-
-const Role = auth.role;
 
 connectDB()
   .then(() => {
     console.log("connected db server");
-    initial();
+    initialDB();
   })
   .then(() => {
     bootServer();
@@ -21,42 +16,6 @@ connectDB()
     console.log(err);
     process.exit();
   });
-
-const initial = () => {
-  Role.estimatedDocumentCount((err, count) => {
-    if (!err && count === 0) {
-      new Role({
-        name: "user"
-      }).save(err => {
-        if (err) {
-          console.log("error", err);
-        }
-
-        console.log("added 'user' to roles collection");
-      });
-
-      new Role({
-        name: "moderator"
-      }).save(err => {
-        if (err) {
-          console.log("error", err);
-        }
-
-        console.log("added 'moderator' to roles collection");
-      });
-
-      new Role({
-        name: "admin"
-      }).save(err => {
-        if (err) {
-          console.log("error", err);
-        }
-
-        console.log("added 'admin' to roles collection");
-      });
-    }
-  });
-}
 
 const bootServer = () => {
   const app = express();
