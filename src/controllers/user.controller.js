29340e0bf9881ef7bde612711a0bf8b1546c2e77
mvn-habui-hn <ha.bui@monstar-lab.com>
2022-04-26 09:41:27 +0700

    update avatar

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,56 +4,57 @@ import { uploadImage } from '../apis/imgBB.api';
 import { UserModel } from '../models/user.model';
 
 const allAccess = (req, res) => {
-	res.status(200).send('Public Content.');
+  res.status(200).send('Public Content.');
 };
 
 const userBoard = (req, res) => {
-	res.status(200).send('User Content.');
+  res.status(200).send('User Content.');
 };
 
 const adminBoard = (req, res) => {
-	res.status(200).send('Admin Content.');
+  res.status(200).send('Admin Content.');
 };
 
 const moderatorBoard = (req, res) => {
-	res.status(200).send('Moderator Content.');
+  res.status(200).send('Moderator Content.');
 };
 
 const updateUser = async (req, res) => {
-	try {
-		const { id } = req.params;
-		const result = await userService.updateUser(id, req.body);
-		res.status(httpStatusCode.OK).json({ result: result });
-	} catch (error) {
-		res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({ message: new Error(error).message });
-	}
+  try {
+    const { id } = req.params;
+    const result = await userService.updateUser(id, req.body);
+    res.status(httpStatusCode.OK).json({ result: result });
+  } catch (error) {
+    res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({ message: new Error(error).message });
+  }
 };
 
 const updateAvatar = async (req, res) => {
-	try {
-		const { id, avatar } = req.body;
-		await uploadImage(avatar).then((result) => {
-			UserModel.findOneAndUpdate(
-				{
-					_id: id,
-				},
-				{
-					avatar_url: result,
-				}
-			);
-			//validate
-			res.status(200).json({ result: result });
-		});
-	} catch (error) {
-		res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({ message: new Error(error).message });
-	}
+  try {
+    const { id, avatar } = req.body;
+    await uploadImage(avatar).then(async result => {
+      await UserModel.findOneAndUpdate(
+        {
+          _id: id
+        },
+        {
+          avatar_url: result
+        }
+      );
+      //validate
+      res.status(200).json({ result: result });
+    });
+  } catch (error) {
+    res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({ message: new Error(error).message });
+  }
 };
 
 export const userController = {
-	allAccess,
-	userBoard,
-	adminBoard,
-	moderatorBoard,
-	updateUser,
-	updateAvatar,
+  allAccess,
+  userBoard,
+  adminBoard,
+  moderatorBoard,
+  updateUser,
+  updateAvatar
 };
+
